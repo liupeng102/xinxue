84c27514e994aa42d37177cb3dd5e0517dc65daa
liuxing <62691415+liupeng102@users.noreply.github.com>
2022-07-05 17:51:54 +0800

    Add files via upload

---
+++ wect/src/main/java/com/example/wect/WectApplication.java
@@ -0,0 +1,13 @@
+package com.example.wect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WectApplication.class, args);
+    }
+
+}
---
+++ wect/src/test/java/com/example/wect/WectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
